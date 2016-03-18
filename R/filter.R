19598280158d407f44dcf317b7e2b61a824709f8
Esa Junttila <esa.junttila@gmail.com>
2016-03-18 21:33:20 +0200

    finish dominant color measures (not sure if correct)

--- R/filter.R
+++ R/filter.R
@@ -630,58 +630,82 @@ mdweVertical <- function(img) {
 # Sonia Ouni, Ezzeddine Zagrouba, Majed Chambah, 2012
 # International Journal of Computer Applications (0975 ? 8887)
 # Volume 40? No.17, February 2012
+
 # Note that there are errors in the RGB->HSV conversion formula. See Wikipedia instead.
+# The article contains numerous errors, the examples don't match with definitions,
+# and the exact methods are sometimes vague. I've done my best to develop a method
+# that is both practical and close to the spirit of the authors' ideas.
+
+# COMMENTS:
+
+## Kappa is the same as the following (measure of "compactness"):
+#C <- sum(cos(hues[mask]))
+#S <- sum(sin(hues[mask]))
+#R <- sqrt(C^2 + S^2)
+#n <- length(hues[mask])
+#kappa <- A1inv(R/n)
+
+## The exact computation of DS leads to a huge computational task.
+#k <- length(row.idx)
+#ds <- 0
+#for (i in 1:k) {
+#  local.dist <- sum(sqrt((row.idx - row.idx[i])^2 + (col.idx - col.idx[i])^2)) / k
+#  ds <- ds + local.dist
+#}
+#spatial.dispersion <- ds / k
+## OR
+#spatial.dispersion <- sum(sapply(1:length(row.idx), function(i) sqrt((row.idx - row.idx[i])^2 + (col.idx - col.idx[i])^2))) / npdc^2
+
+## This is an average distance between pixels that have dominant color.
+## I still think the value should be normalized somehow, for example dividing
+## by an average distance between two pixels in the whole image, not just
+## in dominant color. Otherwise image size has an effect on the absolute
+## magnitude of the number. In the original article the values
+## like 0.0020936 don't quite match with the definitions.
+
 dispersionDominantColor <- function(img.hsv) {
   hues <- degreeToRadian(extractHSVChannel(img.hsv, HUE))  # convert from degrees to radians
-  mask <- !is.na(hues)  # existing hues
+  nr <- nrow(hues)
+  nc <- ncol(hues)
+  mask <- !is.na(hues)  # existing hues, excluding white and black
   mu <- atan2(sum(sin(hues[mask])), sum(cos(hues[mask])))%%(2*pi)  # angles between hues
-  # Next we should estimate the kappa parameter of a von Mises distribution in a circular domain.
-  #print(paste('mu (degrees):', radianToDegree(mu)))
+  # Estimate kappa parameter on a von Mises distribution in a circular domain.
   kappa <- A1inv(mean(cos(hues[mask] - mu)))
 
-  ## Same as the following:
-  #C <- sum(cos(hues[mask]))
-  #S <- sum(sin(hues[mask]))
-  #R <- sqrt(C^2 + S^2)
-  #n <- length(hues[mask])
-  #kappa <- A1inv(R/n)
-
-  # NPDC and pi measure:
+  # NPDC and pi measures
+  ######################
+  # distances between hues (in radians)
   dist.raw <- abs(hues[mask] - mu)
   dist <- pmin(dist.raw, 2*pi - dist.raw)  # distance between hues, mod 2*pi
-  npdc.mask <- dist <= kappa
+  npdc.mask <- dist <= 1/kappa  # article has a different interpretation of 1/kappa ??
   npdc <- sum(npdc.mask)  # pixels in a dominant color (within range kappa)
   pi.measure <- npdc / length(hues[mask])
-  px.row <- matrix(1:length(hues) %% nrow(hues), nrow=nrow(hues))
-  px.col <- matrix(1:length(hues) %/% nrow(hues), nrow=nrow(hues))
+  px.row <- matrix(1:length(hues) %% nr, nrow=nr)
+  px.col <- matrix(1:length(hues) %/% nr, nrow=nr)
   
-  # THE FOLLOWING HAS NOT BEEN TESTED:
-  # Local distance LD:
+  # Spatial dispersion, by approximation
+  ######################################
   row.idx <- c(px.row[mask][npdc.mask])
   col.idx <- c(px.col[mask][npdc.mask])
-  #spatial.dispersion <- sum(sapply(1:length(row.idx), function(i) sqrt((row.idx - row.idx[i])^2 + (col.idx - col.idx[i])^2))) / npdc^2
+  npdc.nr <- length(row.idx)
+  npdc.nc <- length(col.idx)
 
-  k <- length(row.idx)
-  ds <- 0
-  for (i in 1:k) {
-    local.dist <- sum(sqrt((row.idx - row.idx[i])^2 + (col.idx - col.idx[i])^2)) / k
-    ds <- ds + local.dist
-  }
-  spatial.dispersion <- ds / k  # average distance between pixels that have dominant color
-  # I still think the value should be normalized somehow, for example dividing
-  # by an average distance between wo pixinls in the whole image, not just in dominan color.
-  # Otherwise image size has an effect on the absolute maghnitude of the number.
-  # In the original article the values like 0.0020936 don't make much sense.
+  # Approximation for all Euclidean distances, for custom normalization
+  num.samples <- 10000  # just enough to get a hint about the avg distance
+  smpl <- function(values) sample(values, num.samples, replace=TRUE)
+  approx.dist <- mean(sqrt((smpl(1:nr) - smpl(1:nr))^2 + (smpl(1:nc) - smpl(1:nc))^2))
 
-  # Results:
-  print(paste('kappa',kappa))
-  print(paste('npdc', npdc))
-  print(paste('length(hues)', length(hues)))
-  print(paste('spatial.dispersion', spatial.dispersion))
-  
-  #circ.data <- circular(hues[mask], units='radians', zero=0, rotation="clock")  # debug
-  return(list(mu=mu, kappa=kappa, pi=pi.measure, ds=spatial.dispersion))
+  # NPDC Euclidean distance approximation by random sample
+  px.1 <- smpl(1:npdc.nr)
+  px.2 <- smpl(1:npdc.nr)
+  npdc.approx.dist <- mean(sqrt((row.idx[px.1] - row.idx[px.2])^2 + (col.idx[px.1] - col.idx[px.2])^2))
+
+  spatial.dispersion <- npdc.approx.dist
+  custom.ds <- min(1, npdc.approx.dist / approx.dist)  # cap to 1 (exceeds by random variation)
+
+  return(list(mu=mu, kappa=kappa, pi=pi.measure, ds=spatial.dispersion, custom.ds=custom.ds))
 }
+
 
 
 ##########
@@ -699,8 +723,22 @@ main <- function() {
   #filename <- '../examples/K5_10994.JPG'
   #filename <- '../examples/green_grass_blue_sky.png'
   #filename <- '../examples/dark_city.png'
+  #filename <- '../examples/violetred.png'
+  #filename <- '../examples/bluehue.png'
+  #filename <- '../examples/temple_set/temple-a-original.png'
+  #filename <- '../examples/temple_set/temple-b-blue.png'
+  #filename <- '../examples/temple_set/temple-c-cyan.png'
+  #filename <- '../examples/temple_set/temple-d-yellow.png'
+  #filename <- '../examples/temple_set/temple-e-magenta.png'
+  #filename <- '../examples/temple_set/temple-f-red.png'
+  #filename <- '../examples/temple_set/temple-g-green.png'
+  #filename <- '../examples/temple_set/temple-h-noise.png'
+  #filename <- '../examples/temple_set/temple-i-contrast.png'
+  #filename <- '../examples/temple_set/temple-j-colornoise.png'
+  #filename <- '../examples/temple_set/temple-k-gaussianblur.png'
   
   img <- readImage(filename)
+  print(paste('Processing image:', filename))
   for (channel in RGB) {
     if (do.view) view(extractRGBChannel(img,channel), title=paste(channel$name, 'color channel'))
   }
@@ -726,10 +764,18 @@ main <- function() {
   print(paste('MDWE Gaussian quality (0--1):', mdwe.score[['gaussian']]))  # greater is better
   print(paste('MDWE JPEG2000 quality (0--1):', mdwe.score[['jpeg2k']]))  # greater is better
   ddc <- dispersionDominantColor(toHSV(img))
+
   # Dominant direction, spread, and portion of the dominant color.
+  # Dominant hue as an angle
   print(paste('Color dispersion(mu):', ddc$mu, 'and in degrees', radianToDegree(ddc$mu)))
+  # Compactness of hues, larger kappa means narrower hues
   print(paste('Color dispersion(kappa):', ddc$kappa))
+  # Proportion of pixels whose hue is close to dominant
   print(paste('Color dispersion(pi):', ddc$pi))
+  # Spatial dispersion: mean distance of pixels that are dominant
+  print(paste('Color dispersion(ds):', ddc$ds, 'px'))
+  # Custom normalized spatial dispersion: dominantdistances / alldistances
+  print(paste('Color dispersion(custom.ds):', ddc$custom.ds))
   if (do.view) viewDDC(filename='polarcolor.png', toHSV(img), ddc$mu, ddc$kappa)
   print('===  END  ===')
 }
