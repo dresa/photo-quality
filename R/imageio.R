7bd5b11a6943de89142f632a8ea6261428663b1d
Esa Junttila <esa.junttila@gmail.com>
2017-08-29 00:33:13 +0300

    ignore Alpha channel in RGBA if fully opaque

--- R/imageio.R
+++ R/imageio.R
@@ -8,6 +8,7 @@ library(png)   # function 'readPNG' to read PNG files
 
 # Read image file (either a valid PNG or JPEG extension required)
 readImage <- function(filename) {
+  ALPHA <- 4
   refname <- tolower(trimws(filename))
   # Does text end with any of given suffixes? Sensitive to regex chars!
   endsWithAny <- function(txt, suffixes) {
@@ -22,6 +23,15 @@ readImage <- function(filename) {
   if (length(dim(img)) == 2) {  # only one value per pixel
     # Image contains only greyscale values; repeat values to create RGB channels.
     img <- array(img, dim=c(dim(img), 3))
+  }
+  if (dim(img)[3] == ALPHA) {
+    # alpha channel detected
+    if (all(img[ , , ALPHA] == 1.0)) {  # are we fully opaque? If yes, then alpha can be ignored.
+      img <- img[ , , -ALPHA]  # drop the ALPHA channel without effect
+    } else {
+      stop(paste('Error: Alpha channel in img', filename, 'has non-opaque values (<1.0).'))
+      # If this happens, we need to adjust theimage  by mixing in some background color.
+    }
   }
   dimnames(img) <- list(NULL, NULL, c('Red', 'Green', 'Blue'))
   return(img)
