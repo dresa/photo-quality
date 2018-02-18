d990ff8c31d8ab4d6d4c3ef98291a2418d9894a2
Esa Junttila <esa.junttila@gmail.com>
2018-02-18 18:01:17 +0200

    add new quality measures to data analysis methodology

--- training-data/traininganalysis.R
+++ training-data/traininganalysis.R
@@ -1,27 +1,57 @@
 # Analyzing trainign results and correlations
 #
-# Now around 0.3 correlation after linear modeling. Should be able to do better.
+# Now correlations around 0.59 (for B&W) and 0.46 (for colored images) after linear modeling. Should be able to do better.
 # Add more measures:
-# - photo saturation, average/median?
-# - dimensions or photo aspect ratio
-# - photo size (+ or x)
 # - fine-tune existing measures: exposure and jpeg2k, DDC compacness (Inf-->BW?)
 # - intervalcontrast with different intervals: 95% vs ?
-# - vs. standard deviation? (instead of rating?)
-# - Ritendra Datta: Studying Aesthetics in Photographic Images Using a Computational Approach
+# - vs. standard deviation? (instead of rating?). The avg rating is not objective truth; the distribution of ratings is.
+# - other scientific papers
+# - remove unnecessary measures that have power for predictions
 #
 # Esa Junttila, 2017-08-26 (originally)
 
 dpchallenge <- read.table('dpchallenge_dataset.txt', header=TRUE)
-measures <- read.table('measures_16504.csv', header=TRUE, sep=';', stringsAsFactors=FALSE)
+measures <- read.table('measures_datta_first_1000.csv', header=TRUE, sep=';', stringsAsFactors=FALSE)
 
 # All photos
 df <- merge(dpchallenge, measures, by='Photo')
 print('All photos correlations:')
-print(cor(df[, c('Rating', 'Blur', 'MdweBlur', 'MdweGaussian', 'MdweJpeg2k', 'Exposure', 'RmsContrast', 'IntervalContrast')]))
-plot(df$IntervalContrast, df$Rating)
+print(cor(df[, c('Rating', 'Blur', 'MdweBlur', 'MdweGaussian', 'MdweJpeg2k', 'Exposure', 'RmsContrast', 'IntervalContrast',
+                 'AverageIntensityD01', 'ColorfulnessD02', 'ColorfulnessGreyD02E',
+                 'AverageSaturationD03', 'AverageHueD04',
+                 'AverageCentralHueD05', 'AverageCentralSaturationD06', 'AverageCentralIntensityD07',
+                 'TextureHueLevel1D10', 'TextureHueLevel2D11', 'TextureHueLevel3D12', 
+                 'TextureSatLevel1D13', 'TextureSatLevel2D14', 'TextureSatLevel3D15', 
+                 'TextureValLevel1D16', 'TextureValLevel2D17', 'TextureValLevel3D18', 
+                 'TextureHueSumD19', 'TextureSatSumD20', 'TextureValSumD21', 
+                 'SizeFeatureD22', 'AspectRatioD23', 'AspectRatioMaxD23E', 
+                 'NumLargePatchesD24', 'NumClustersD25',
+                 'AvgHuePatch1D26', 'AvgHuePatch2D27', 'AvgHuePatch3D28', 'AvgHuePatch4D29', 'AvgHuePatch5D30', 
+                 'AvgSatPatch1D31', 'AvgSatPatch2D32', 'AvgSatPatch3D33', 'AvgSatPatch4D34', 'AvgSatPatch5D35', 
+                 'AvgValPatch1D36', 'AvgValPatch2D37', 'AvgValPatch3D38', 'AvgValPatch4D39', 'AvgValPatch5D40', 
+                 'RelSizePatch1D41', 'RelSizePatch2D42', 'RelSizePatch3D43', 'RelSizePatch4D44', 'RelSizePatch5D45', 
+                 'PositionPatch1D48', 'PositionPatch2D49', 'PositionPatch3D50', 'PositionPatch4D51', 'PositionPatch5D52', 
+                 'DistanceFromCenterPatch1D48E', 'DistanceFromCenterPatch2D49E',
+                 'DistanceFromCenterPatch3D50E', 'DistanceFromCenterPatch4D51E', 'DistanceFromCenterPatch5D52E', 
+                 'DepthOfFieldHueD53', 'DepthOfFieldSatD54', 'DepthOfFieldValD55', 
+                 'ShapeConvexityD56'
+)]))
+#plot(df$IntervalContrast, df$Rating)
 hist(df$Blur, breaks=20)
-model <- lm(df$Rating ~ df$Blur + df$MdweBlur + df$MdweGaussian + df$MdweJpeg2k + df$Exposure + df$RmsContrast + df$IntervalContrast)
+model <- lm(df$Rating ~ df$Blur + df$MdweBlur + df$MdweGaussian + df$MdweJpeg2k + df$Exposure + df$RmsContrast + df$IntervalContrast +
+            df$AverageIntensityD01 + df$ColorfulnessD02 + df$ColorfulnessGreyD02E +
+            df$AverageCentralIntensityD07 +
+            df$TextureValLevel1D16 + df$TextureValLevel2D17 + df$TextureValLevel3D18 + 
+            df$TextureValSumD21 +
+            df$SizeFeatureD22 + df$AspectRatioD23 + df$AspectRatioMaxD23E + 
+            df$NumLargePatchesD24 + df$NumClustersD25 +
+            df$AvgValPatch1D36 + df$AvgValPatch2D37 + df$AvgValPatch3D38 + df$AvgValPatch4D39 + df$AvgValPatch5D40 +
+            df$RelSizePatch1D41 + df$RelSizePatch2D42 + df$RelSizePatch3D43 + df$RelSizePatch4D44 + df$RelSizePatch5D45 +
+            df$PositionPatch1D48 + df$PositionPatch2D49 + df$PositionPatch3D50 + df$PositionPatch4D51 + df$PositionPatch5D52 +
+            df$DistanceFromCenterPatch1D48E + df$DistanceFromCenterPatch2D49E + 
+            df$DistanceFromCenterPatch3D50E + df$DistanceFromCenterPatch4D51E + df$DistanceFromCenterPatch5D52E +
+            df$DepthOfFieldValD55 + 
+            df$ShapeConvexityD56)
 modeled.ratings <- predict(model)
 print(cor(df$Rating, modeled.ratings))
 sapply(colnames(df), function(x) {
@@ -30,44 +60,88 @@ sapply(colnames(df), function(x) {
   abline(lm(df$Rating[mask] ~ df[[x]][mask]), col='red')
 })
 
+
 # Color photos
-is.bw <- is.na(df$DdcCompactness) | df$DdcCompactness < 1e-5 | is.infinite(df$DdcCompactness)
+##############
+is.bw <- is.na(df$DdcCompactness) | df$DdcCompactness < 1e-5 | is.infinite(df$DdcCompactness) | df$AverageCentralSaturationD06 < 1e-5
 colored <- df[!is.bw, ]
 print('Colored correlations:')
-print(cor(colored[, c('Rating', 'Blur', 'MdweBlur', 'MdweGaussian', 'MdweJpeg2k', 'Exposure', 'RmsContrast', 'IntervalContrast', 'DdcMean', 'DdcCompactness', 'DdcDominance', 'DdcSpatial', 'DdcNormSpatial')]))
-colored.model <- lm(colored$Rating ~ colored$Blur + colored$MdweBlur + colored$MdweGaussian + colored$MdweJpeg2k + colored$Exposure + colored$RmsContrast + colored$IntervalContrast + colored$DdcMean + colored$DdcCompactness + colored$DdcDominance + colored$DdcSpatial + colored$DdcNormSpatial)
+print(cor(colored[, c('Rating', 'Blur', 'MdweBlur', 'MdweGaussian', 'MdweJpeg2k', 'Exposure', 'RmsContrast', 'IntervalContrast',
+                      'AverageIntensityD01', 'ColorfulnessD02', 'ColorfulnessGreyD02E',
+                      'AverageSaturationD03', 'AverageHueD04',
+                      'AverageCentralHueD05', 'AverageCentralSaturationD06', 'AverageCentralIntensityD07',
+                      'TextureHueLevel1D10', 'TextureHueLevel2D11', 'TextureHueLevel3D12', 
+                      'TextureSatLevel1D13', 'TextureSatLevel2D14', 'TextureSatLevel3D15', 
+                      'TextureValLevel1D16', 'TextureValLevel2D17', 'TextureValLevel3D18', 
+                      'TextureHueSumD19', 'TextureSatSumD20', 'TextureValSumD21', 
+                      'SizeFeatureD22', 'AspectRatioD23', 'AspectRatioMaxD23E', 
+                      'NumLargePatchesD24', 'NumClustersD25',
+                      'AvgHuePatch1D26', 'AvgHuePatch2D27', 'AvgHuePatch3D28', 'AvgHuePatch4D29', 'AvgHuePatch5D30', 
+                      'AvgSatPatch1D31', 'AvgSatPatch2D32', 'AvgSatPatch3D33', 'AvgSatPatch4D34', 'AvgSatPatch5D35', 
+                      'AvgValPatch1D36', 'AvgValPatch2D37', 'AvgValPatch3D38', 'AvgValPatch4D39', 'AvgValPatch5D40', 
+                      'RelSizePatch1D41', 'RelSizePatch2D42', 'RelSizePatch3D43', 'RelSizePatch4D44', 'RelSizePatch5D45', 
+                      'PositionPatch1D48', 'PositionPatch2D49', 'PositionPatch3D50', 'PositionPatch4D51', 'PositionPatch5D52', 
+                      'DistanceFromCenterPatch1D48E', 'DistanceFromCenterPatch2D49E',
+                      'DistanceFromCenterPatch3D50E', 'DistanceFromCenterPatch4D51E', 'DistanceFromCenterPatch5D52E', 
+                      'DepthOfFieldHueD53', 'DepthOfFieldSatD54', 'DepthOfFieldValD55', 
+                      'ShapeConvexityD56',
+                      'DdcMean', 'DdcCompactness', 'DdcDominance', 'DdcSpatial', 'DdcNormSpatial')]))
+colored.model <- lm(colored$Rating ~ colored$Blur + colored$MdweBlur + colored$MdweGaussian + colored$MdweJpeg2k + colored$Exposure + colored$RmsContrast + colored$IntervalContrast +
+                      colored$AverageIntensityD01 + colored$ColorfulnessD02 + colored$ColorfulnessGreyD02E +
+                      colored$AverageSaturationD03 + colored$AverageHueD04 +
+                      colored$AverageCentralHueD05 + colored$AverageCentralSaturationD06 + colored$AverageCentralIntensityD07 +
+                      colored$TextureHueLevel1D10 + colored$TextureHueLevel2D11 + colored$TextureHueLevel3D12 + 
+                      colored$TextureSatLevel1D13 + colored$TextureSatLevel2D14 + colored$TextureSatLevel3D15 + 
+                      colored$TextureValLevel1D16 + colored$TextureValLevel2D17 + colored$TextureValLevel3D18 + 
+                      colored$TextureHueSumD19 + colored$TextureSatSumD20 + colored$TextureValSumD21 +
+                      colored$SizeFeatureD22 + colored$AspectRatioD23 + colored$AspectRatioMaxD23E + 
+                      colored$NumLargePatchesD24 + colored$NumClustersD25 +
+                      colored$AvgHuePatch1D26 + colored$AvgHuePatch2D27 + colored$AvgHuePatch3D28 + colored$AvgHuePatch4D29 + colored$AvgHuePatch5D30 +
+                      colored$AvgSatPatch1D31 + colored$AvgSatPatch2D32 + colored$AvgSatPatch3D33 + colored$AvgSatPatch4D34 + colored$AvgSatPatch5D35 +
+                      colored$AvgValPatch1D36 + colored$AvgValPatch2D37 + colored$AvgValPatch3D38 + colored$AvgValPatch4D39 + colored$AvgValPatch5D40 +
+                      colored$RelSizePatch1D41 + colored$RelSizePatch2D42 + colored$RelSizePatch3D43 + colored$RelSizePatch4D44 + colored$RelSizePatch5D45 +
+                      colored$PositionPatch1D48 + colored$PositionPatch2D49 + colored$PositionPatch3D50 + colored$PositionPatch4D51 + colored$PositionPatch5D52 +
+                      colored$DistanceFromCenterPatch1D48E + colored$DistanceFromCenterPatch2D49E + 
+                      colored$DistanceFromCenterPatch3D50E + colored$DistanceFromCenterPatch4D51E + colored$DistanceFromCenterPatch5D52E +
+                      colored$DepthOfFieldHueD53 + colored$DepthOfFieldSatD54 + colored$DepthOfFieldValD55 + 
+                      colored$ShapeConvexityD56 +
+                      colored$DdcMean + colored$DdcCompactness + colored$DdcDominance + colored$DdcSpatial + colored$DdcNormSpatial)
 print(cor(colored$Rating, predict(colored.model)))
+#write.table(cor(colored), file='correlations_1000_colored.csv')
+
 
 # Black & White photos
+######################
 bw <- df[is.bw, ]
 print('BW correlations:')
-print(cor(bw[, c('Rating', 'Blur', 'MdweBlur', 'MdweGaussian', 'MdweJpeg2k', 'Exposure', 'RmsContrast', 'IntervalContrast')]))
-bw.model <- lm(bw$Rating ~ bw$Blur + bw$MdweBlur + bw$MdweGaussian + bw$MdweJpeg2k + bw$Exposure + bw$RmsContrast + bw$IntervalContrast)
+print(cor(bw[, c('Rating', 'Blur', 'MdweBlur', 'MdweGaussian', 'MdweJpeg2k', 'Exposure', 'RmsContrast', 'IntervalContrast',
+                 'AverageIntensityD01', 'ColorfulnessD02', 'ColorfulnessGreyD02E',
+                 'AverageCentralIntensityD07',
+                 'TextureValLevel1D16', 'TextureValLevel2D17', 'TextureValLevel3D18', 
+                 'TextureValSumD21', 
+                 'SizeFeatureD22', 'AspectRatioD23', 'AspectRatioMaxD23E', 
+                 'NumLargePatchesD24', 'NumClustersD25',
+                 'AvgValPatch1D36', 'AvgValPatch2D37', 'AvgValPatch3D38', 'AvgValPatch4D39', 'AvgValPatch5D40', 
+                 'RelSizePatch1D41', 'RelSizePatch2D42', 'RelSizePatch3D43', 'RelSizePatch4D44', 'RelSizePatch5D45', 
+                 'PositionPatch1D48', 'PositionPatch2D49', 'PositionPatch3D50', 'PositionPatch4D51', 'PositionPatch5D52', 
+                 'DistanceFromCenterPatch1D48E', 'DistanceFromCenterPatch2D49E',
+                 'DistanceFromCenterPatch3D50E', 'DistanceFromCenterPatch4D51E', 'DistanceFromCenterPatch5D52E', 
+                 'DepthOfFieldValD55', 
+                 'ShapeConvexityD56'
+                 )]))
+bw.model <- lm(bw$Rating ~ bw$Blur + bw$MdweBlur + bw$MdweGaussian + bw$MdweJpeg2k + bw$Exposure + bw$RmsContrast + bw$IntervalContrast +
+                 bw$AverageIntensityD01 + bw$ColorfulnessD02 + bw$ColorfulnessGreyD02E +
+                 bw$AverageCentralIntensityD07 +
+                 bw$TextureValLevel1D16 + bw$TextureValLevel2D17 + bw$TextureValLevel3D18 + 
+                 bw$TextureValSumD21 +
+                 bw$SizeFeatureD22 + bw$AspectRatioD23 + bw$AspectRatioMaxD23E + 
+                 bw$NumLargePatchesD24 + bw$NumClustersD25 +
+                 bw$AvgValPatch1D36 + bw$AvgValPatch2D37 + bw$AvgValPatch3D38 + bw$AvgValPatch4D39 + bw$AvgValPatch5D40 +
+                 bw$RelSizePatch1D41 + bw$RelSizePatch2D42 + bw$RelSizePatch3D43 + bw$RelSizePatch4D44 + bw$RelSizePatch5D45 +
+                 bw$PositionPatch1D48 + bw$PositionPatch2D49 + bw$PositionPatch3D50 + bw$PositionPatch4D51 + bw$PositionPatch5D52 +
+                 bw$DistanceFromCenterPatch1D48E + bw$DistanceFromCenterPatch2D49E + 
+                 bw$DistanceFromCenterPatch3D50E + bw$DistanceFromCenterPatch4D51E + bw$DistanceFromCenterPatch5D52E +
+                 bw$DepthOfFieldValD55 + 
+                 bw$ShapeConvexityD56)
 print(cor(bw$Rating, predict(bw.model)))
-
-# Large DDC compactness:
-# Photo Index DdcCompactness DdcDominance DdcSpatial DdcNormSpatial
-# 305   10651 Inf      5.402542e-01 256.991718    0.846980861
-# 1776  13031 Inf      1.000000e+00 123.869782    0.543859279
-# 2003  13494 Inf      9.154412e-01 178.556321    0.625816707
-# 2182  13796 2902.739 5.443135e-05 240.770650    0.819341477
-# 2419  14929 1953.258 2.260852e-04 255.161436    0.772387298
-# 3367  18204 5315.953 3.561198e-02 233.937880    0.797297346
-# 5082  23811 1309.016 8.423497e-03 278.976903    0.887392972
-# 7313  38474 1126.500 9.997780e-01 392.860437    1.000000000
-# 8437  43380 Inf      1.000000e+00   6.389113    0.022834884
-# 10496 53787 1057.933 2.500031e-05  20.897852    0.077369038
-# 11260 57288 Inf      1.000000e+00 135.224579    0.444807833
-# 11350 57928 Inf      1.000000e+00   6.062565    0.020950792
-# 11394 58131 Inf      1.000000e+00 195.242599    0.691562631
-# 11732 59663 2435.836 9.991203e-01 147.846508    1.000000000
-# 11782 59912 1176.419 3.038549e-02 196.329697    0.867218015
-# 12597 62443 1092.586 1.440383e-05   1.659439    0.005981826
-# 12722 62900 Inf      1.000000e+00   4.148135    0.013665845
-# 13067 64130 1262.327 1.351789e-03 204.791085    0.695725103
-# 14677 70749 Inf      7.583333e-01 167.415594    0.555052858
-# 15173 73644 Inf      2.706410e-01 223.055849    0.761133387
-# 15890 76078 3045.047 3.753870e-03 190.062750    0.738364654
-# 16401 79163 1188.873 5.897273e-02 306.207215    1.000000000
-# 16500 79954 Inf      8.666667e-01  23.261137    0.082806309
 
